253b862341d11986836e2abf5874c9468a294741
AgeingHippy <charles_mcdonald@hotmail.com>
2025-02-27 22:34:06 +0000

    Refactoring in progress contemplate DTO

---
+++ src/main/java/com/ageinghippy/controller/FoodTypeController.java
@@ -0,0 +1,69 @@
+package com.ageinghippy.controller;
+
+import com.ageinghippy.model.FoodType;
+import com.ageinghippy.service.FoodTypeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/v1/food-types")
+@RequiredArgsConstructor
+public class FoodTypeController {
+
+    private final FoodTypeService foodTypeService;
+
+    @GetMapping
+    public List<FoodType> getFoodTypes() {
+        return foodTypeService.getFoodTypes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FoodType> getFoodType(@PathVariable Long id) {
+        FoodType foodType = foodTypeService.getFoodType(id);
+        if (foodType != null) {
+            return ResponseEntity.ok(foodType);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<FoodType> postFoodType(@Valid @RequestBody FoodType foodType) {
+        foodType = foodTypeService.saveFoodType(foodType);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(foodType.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(foodType);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FoodType> putFoodType(@RequestBody FoodType foodType, @PathVariable Long id) {
+        try {
+            foodType = foodTypeService.updateFoodType(id, foodType);
+            return ResponseEntity.ok(foodType);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFoodType(@PathVariable Long id) {
+        FoodType foodType = foodTypeService.getFoodType(id);
+        if (foodType != null) {
+            foodTypeService.deleteFoodType(foodType);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
---
+++ src/main/java/com/ageinghippy/service/FoodTypeService.java
@@ -0,0 +1,49 @@
+package com.ageinghippy.service;
+
+import com.ageinghippy.model.FoodType;
+import com.ageinghippy.repository.FoodTypeRepository;
+import com.ageinghippy.util.Util;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FoodTypeService {
+    private final FoodTypeRepository foodTypeRepository;
+
+    public FoodType getFoodType(Long id) {
+        return foodTypeRepository.findById(id).orElse(null);
+    }
+
+    public List<FoodType> getFoodTypes() {
+        return foodTypeRepository.findAll();
+    }
+
+    public FoodType saveFoodType(FoodType foodType) {
+        foodType = foodTypeRepository.save(foodType);
+
+        return foodType;
+    }
+
+    /**
+     * Updates the foodType identified by the provided id with non-null values as provided in updateFoodType
+     *
+     * @param id             The id of the {@code FoodType} to update
+     * @param updateFoodType the {@code FoodType} containing updated values
+     * @return the updated {@code FoodType}
+     * @throws java.util.NoSuchElementException if the FoodType with the provided id does not exist
+     */
+    public FoodType updateFoodType(Long id, FoodType updateFoodType) {
+        FoodType foodType = foodTypeRepository.findById(id).orElseThrow();
+        foodType.setName(Util.valueIfNull(updateFoodType.getName(), foodType.getName()));
+        //todo - repeat as required
+        return saveFoodType(foodType);
+    }
+
+    public void deleteFoodType(FoodType foodType) {
+        foodTypeRepository.deleteById(foodType.getId());
+    }
+}
