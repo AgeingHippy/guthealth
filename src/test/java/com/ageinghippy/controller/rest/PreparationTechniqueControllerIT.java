9453c290d9644d0d4f6c880efc02db1d6ffd98eb
AgeingHippy <charles_mcdonald@hotmail.com>
2025-03-26 18:56:19 +0000

    add security to remaining controller tests

--- src/test/java/com/ageinghippy/controller/rest/PreparationTechniqueControllerIT.java
+++ src/test/java/com/ageinghippy/controller/rest/PreparationTechniqueControllerIT.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@WithUserDetails("basic")
 class PreparationTechniqueControllerIT {
 
     @Autowired
