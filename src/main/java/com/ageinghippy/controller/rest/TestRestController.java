8123748c0584ebea4f124d5b08f44cc507055490
AgeingHippy <charles_mcdonald@hotmail.com>
2025-04-25 13:55:08 +0100

    Testing javascript access to backend

---
+++ src/main/java/com/ageinghippy/controller/rest/TestRestController.java
@@ -0,0 +1,27 @@
+package com.ageinghippy.controller.rest;
+
+import com.ageinghippy.model.CustomUserPrincipal;
+import com.ageinghippy.model.entity.UserPrinciple;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+    @GetMapping("/time")
+    public String getTest(Authentication authentication) {
+        String userName = "Unknown User";
+        if (authentication != null) {
+            UserPrinciple userPrinciple = ((CustomUserPrincipal) authentication.getPrincipal()).getUserPrinciple();
+            userName = userPrinciple.getUsername();
+        }
+
+        return MessageFormat.format("{0} accessed server on {1}", userName  ,(new Date()).toString());
+    }
+}
